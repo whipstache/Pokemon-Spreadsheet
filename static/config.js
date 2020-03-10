c4c9ce3b64050ad4cdf0ef669ab1278d712978d8
whipstache <rhennesy@gmail.com>
2020-03-10 16:03:42 -0400

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,21 +2,21 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1K52dcWIP979k5dhVxbeR3mjdeanBpwbjqaMnQH4fVy0";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "SW-0561-6992-5120";
+var inGameName = "Pix";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "https://discordapp.com/users/whipstache#7777";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://static.tumblr.com/c325c33dbca1b0f02c4f987ba72d8b1a/28amrxm/EDQoj61aw/tumblr_static_f42vxiita884ccsgw0w0wsok8.png";
